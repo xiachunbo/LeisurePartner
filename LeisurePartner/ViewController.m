d07e7f364e62c53cb3d1d7bbe610f0c2b0705b7c
xiachunbo <xiachunbo@tsing-cloud.com>
2019-01-23 13:26:35 +0800

    Initial Commit

---
+++ LeisurePartner/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  LeisurePartner
+//
+//  Created by lovebobo on 23/1/19.
+//  Copyright © 2019年 lovebobo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ LeisurePartner/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LeisurePartner
+//
+//  Created by lovebobo on 23/1/19.
+//  Copyright © 2019年 lovebobo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
